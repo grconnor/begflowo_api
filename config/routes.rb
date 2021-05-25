fc7bad962f7da3226649f6b56d948b9426c2cefa
Connor Roelofsen <connorroelofsen15@gmail.com>
2021-05-25 16:38:41 +0200

    Move devise mount in routes. Edit wrap param and remove json.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
   namespace :api do
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
     end
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
       resources :car_data, only: [:create, :index]
     end
   end
